ad6eb88fde8abbf391801dd20f819f49fe7bcba9
Zainan Victor Zhou <zzn+github@zzn.im>
2022-11-18 14:15:29 -0800

    Fix bugs for upgradability

--- src/tasks/depver.ts
+++ src/tasks/depver.ts
@@ -1,5 +1,3 @@
-import { ethers, upgrades } from "hardhat";
-
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import '@nomicfoundation/hardhat-toolbox';
@@ -8,34 +6,39 @@ import '@nomiclabs/hardhat-ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
 const deployByName = async function (
-    ethers: HardhatRuntimeEnvironment["ethers"],
+    hre: HardhatRuntimeEnvironment,
     contractName: string,
     signer: SignerWithAddress,
     isUpgradable: boolean,
 ): Promise<any> /*deployed address*/ {
     let contract;
+    let ethers = hre.ethers;
+    let upgrades = hre.upgrades;
+    console.log(`Deploying signer: ${signer.address}`);
     if (isUpgradable) {
-        const contractFactory = await ethers.getContractFactory(contractName);
+        console.log(`Deploying ${contractName} as upgradable`);
+        const contractFactory = await ethers.getContractFactory(contractName, signer);
         contract = await upgrades.deployProxy(contractFactory);
+        await contract.deployed();
     } else {
+        console.log(`Deploying ${contractName} as non-upgradable`);
         const contractFactory = await ethers.getContractFactory(contractName, signer);
         contract = await contractFactory.deploy();
         await contract.deployed();
     }
-
     let tx = contract.deployTransaction;
     return { contract, tx };
 }
 
 task('depver', 'Deploy and verify the contracts')
-    .addFlag('isUpgradable', 'Whether we are deploying upgradable contracts')
+    .addFlag('upgradable', 'Deploy upgradable contracts')
     .addParam("signerIndex", "Index of signer, default 4")
     .addParam("contractName", "Name of the contract to be deployed.")
     .setAction(async (args, hre: HardhatRuntimeEnvironment) => {
         await hre.run('compile');
         const contractName = args.contractName;
         const signer = (await hre.ethers.getSigners())[parseInt(args.signerIndex)] as SignerWithAddress;
-        const { contract, tx } = await deployByName(hre.ethers, args.contractName, signer, args.isUpgradable);
+        const { contract, tx } = await deployByName(hre, args.contractName, signer, args.upgradable);
         for (let i = 0; i < 6; i++) {
             console.log(`Block ${i}...`);
             await tx.wait(i);
